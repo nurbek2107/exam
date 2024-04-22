d0e889aa3af505af2a1ce593e608e670548e3e00
nurbek2107 <abduaxatov002@gmail.com>
2024-04-22 16:48:29 +0500

    main.js
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

---
+++ main.js
@@ -0,0 +1,86 @@
+// 1-masala
+
+// function arr(str) {
+//     return str.split('').map(Number).reverse();
+// }
+// console.log(arr("34567"))
+
+// 2-masala
+
+// let a = "5"
+// let b = 5
+// function func(){
+// if (a == b) {
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+// }
+// func()
+
+// let c = "5"
+// let d = 5
+// function func() {
+//     if (a === b) {
+//         console.log("true")
+//     } else {
+//         console.log("false")
+//     }
+// }
+// func()
+
+
+// 3-misol
+// let a = 1
+// let arr = []
+// for (let i = 0; i < 10; i++) {
+//     arr.push(a++)
+// }
+// console.log(arr)
+
+// 4-misol
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let toq = 0
+// let sum = 0
+// let res = []
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//         sum +=arr[i]
+//     } else {
+//         toq += arr[i]
+//     }
+// }
+// res.push(sum, toq)
+// console.log(res)
+
+// 5-misol
+
+// function exclamationRemover(str) {
+//     let a = str.split("!");
+//     return a.join("");
+// }
+
+// console.log(exclamationRemover("Bu yerda !! !!! yozilgan !!! !"));
+
+// 6-misol
+
+// let arr = [1, 2, 3, 4, 5]
+// let arr2 = [6, 7, 8, 9, 10]
+// let sum = 0
+// function func(arr, arr2) {
+//     let sum = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i]
+//     }
+//     for (let i = 0; i < arr2.length; i++) {
+//         sum += arr2[i]
+//     }
+//     return sum
+// }
+// console.log(func(arr, arr2))
+// func(arr, arr2,  sum)
+
+
+// 7-misol
+
+
